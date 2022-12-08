46d6fac6d73600f8e0dfda403e326276070ee985
David Tonoyan <davidtonoyan10@me.com>
2022-12-08 22:25:29 +0400

    Add second module

---
+++ taskList/AddModule/AddEntityViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddEntityViewController.swift
+//  taskList
+//
+//  Created by Давид Тоноян  on 08.12.2022.
+//
+
+import UIKit
+
+final class AddEntityViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
