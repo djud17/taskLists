3a3e7b966c57ed1a222981bf720fbd335168c504
David Tonoyan <davidtonoyan10@me.com>
2022-12-08 20:24:26 +0400

    Add Router

---
+++ taskList/TaskListModule/Router/ListRouter.swift
@@ -0,0 +1,28 @@
+//
+//  ListRouter.swift
+//  taskList
+//
+//  Created by Давид Тоноян  on 08.12.2022.
+//
+
+import Foundation
+
+protocol RouterProtocol {
+    
+}
+
+final class ListRouter: RouterProtocol {
+    
+}
+
+/*
+class ProfileRouter {
+
+    weak var navigationController: UINavigationController?
+
+    func openSettings() {
+        let vc = SettingsViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+*/
