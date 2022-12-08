fc5e98e2ced4a064314dac57a37945efba59fa19
David Tonoyan <davidtonoyan10@me.com>
2022-12-08 20:22:14 +0400

    Add Constants file

---
+++ taskList/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  taskList
+//
+//  Created by Давид Тоноян  on 08.12.2022.
+//
+
+import UIKit
+
+enum Constants {
+    enum Colors {
+        static let blue = UIColor(red: 0, green: 0.46, blue: 0.9, alpha: 1)
+        static let white: UIColor = .white
+    }
+    
+    enum Sizes {
+        static let cornerRadius: CGFloat = 10
+    }
+}
