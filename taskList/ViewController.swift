9ca579d794f075f3d3bf24e48cdb5348f431dc79
David Tonoyan <davidtonoyan10@me.com>
2022-12-08 17:58:48 +0400

    Initial Commit

---
+++ taskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskList
+//
+//  Created by Давид Тоноян  on 08.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
