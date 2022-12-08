5d3250925d7ad95be9783093d37d868215e454d1
David Tonoyan <davidtonoyan10@me.com>
2022-12-08 18:09:55 +0400

    Setup pods

--- taskList/ListViewController.swift
+++ taskList/ListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
