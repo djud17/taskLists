0ac1c3bc4ad7a6b62867bf25d73441f9b201d8c6
David Tonoyan <davidtonoyan10@me.com>
2022-12-11 01:50:29 +0400

    Add new constants

--- taskList/Constants.swift
+++ taskList/Constants.swift
@@ -11,9 +11,12 @@ enum Constants {
     enum Colors {
         static let blue = UIColor(red: 0, green: 0.46, blue: 0.9, alpha: 1)
         static let white: UIColor = .white
+        static let red: UIColor = .red
     }
     
     enum Sizes {
         static let cornerRadius: CGFloat = 10
+        static let borderSize: CGFloat = 3
+        static let fontSize: CGFloat = 17
     }
 }
