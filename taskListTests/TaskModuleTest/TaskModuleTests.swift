371223fa1a6c5d2cb4035ebc1c82fb742ee9ac68
David Tonoyan <davidtonoyan10@me.com>
2022-12-13 02:11:51 +0400

    Add tests

--- taskListTests/TaskModuleTest/TaskModuleTests.swift
+++ taskListTests/TaskModuleTest/TaskModuleTests.swift
@@ -6,30 +6,66 @@
 //
 
 import XCTest
+@testable import taskList
+import CoreData
 
 final class TaskModuleTests: XCTestCase {
+    let persistance = PersistanceMock.init()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        persistance.entityMocks = [ListEntityMock.init()]
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        persistance.entityMocks.removeAll()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testInteractorEntity() throws {
+        let entity = ListEntityMock.init()
+        let taskInteractor = TaskInteractor(persistance: persistance, listEntity: entity)
+        XCTAssertEqual(entity.entityName, taskInteractor.listEntity.entityName)
+        XCTAssertTrue(entity === taskInteractor.listEntity)
+        XCTAssertEqual(entity.entityItems.count, taskInteractor.listEntity.entityItems.count)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testInteractorLoadData() throws {
+        let listInteractor = ListInteractor()
+        listInteractor.loadDataFromPersistance()
+        XCTAssertTrue(listInteractor.itemsArray.count == persistance.readFromPersistance().count)
     }
+}
 
+class PersistanceMock: PersistanceProtocol {
+    var converter: taskList.ConverterProtocol = ConverterMock.init()
+    
+    var entityMocks = [ListEntityMock.init()]
+    
+    func writeToPersistance(entity: taskList.EntityProtocol) {
+        
+    }
+    
+    func readFromPersistance() -> [taskList.EntityProtocol] {
+        entityMocks
+    }
+    
+    func deleteFromPersistance(entity: taskList.EntityProtocol) {
+        
+    }
+}
+
+class ConverterMock: ConverterProtocol {
+    func convertToPersistance(data: taskList.EntityProtocol) -> NSManagedObject? {
+        nil
+    }
+    
+    func convertFromPersistance(dataArray: [NSManagedObject]) -> [taskList.EntityProtocol] {
+        []
+    }
+}
+
+class ListEntityMock: EntityProtocol {
+    var entityName: String = "Complete"
+    var entityItems: [taskList.ItemProtocol] = []
 }
